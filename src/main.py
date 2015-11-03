4c6cac520e5aa5b186dc9404a7ff9675dfa0f274
Anton <mordbergak@gmail.com>
2015-11-03 16:57:21 +0300

    added try-finally block

--- src/main.py
+++ src/main.py
@@ -14,36 +14,41 @@ class Users:
 
     def GET(self, id=None):
         db = pg_pool.getconn()
-        cur = db.cursor()
-        if id is None:
-            cur.execute("SELECT * FROM AndroidUser")
-            result = []
-            for row in cur.fetchall():
-                result.append({"id":row[0], "name":row[1]})
-            pg_pool.putconn(db)
-            return json.dumps(result)
-        else:
-            cur.execute("SELECT * FROM AndroidUser WHERE id='{0}'".format(id))
-            user = cur.fetchone()
-            pg_pool.putconn(db)
-            if user is None:
-                return "id={0} not found in database".format(id)
+        result_str = ""
+        try:
+            cur = db.cursor()
+            if id is None:
+                cur.execute("SELECT * FROM AndroidUser")
+                result = []
+                for row in cur.fetchall():
+                    result.append({"id":row[0], "name":row[1]})
+                result_str = json.dumps(result)
             else:
-                return json.dumps({"id":user[0], "name":user[1]})
+                cur.execute("SELECT * FROM AndroidUser WHERE id='{0}'".format(id))
+                user = cur.fetchone()
+                if user is None:
+                    result_str = "id={0} not found in database".format(id)
+                else:
+                    result_str = json.dumps({"id":user[0], "name":user[1]})
+        finally:
+            pg_pool.putconn(db)
+
+        return result_str
 
     def POST(self, name):
         db = pg_pool.getconn()
-        cur = db.cursor()
-        cur.execute("INSERT INTO AndroidUser (name) VALUES ('{0}')".format(name))
-        db.commit()
-        pg_pool.putconn(db)
+        try:
+            cur = db.cursor()
+            cur.execute("INSERT INTO AndroidUser (name) VALUES ('{0}')".format(name))
+            db.commit()
+        finally:
+            pg_pool.putconn(db)
 
 if __name__ == '__main__':
     pg_pool = pool.ThreadedConnectionPool(1, MAX_THREADS, user="tobedone", password="tobedone", host="localhost", dbname="tobedone")
 
     cherrypy.tree.mount(
         Users(), '/api/users',
-        Tasks(), '/api/tasks',
         {'/':
             {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
          }
