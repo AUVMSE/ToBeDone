645ae88c1b8827eba4f95cd92a3aaf6328fd52f2
Egor Gorbunov <egor-mailbox@ya.ru>
2015-11-03 18:54:39 +0300

    Small changes

--- app/src/main/java/org/vmse/spbau/tobedone/MainActivity.java
+++ app/src/main/java/org/vmse/spbau/tobedone/MainActivity.java
@@ -1,4 +1,4 @@
-package org.vmse.spbau.testapp;
+package org.vmse.spbau.tobedone;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -80,18 +81,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_show_task_list) {
+        } else if (id == R.id.nav_settings) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
